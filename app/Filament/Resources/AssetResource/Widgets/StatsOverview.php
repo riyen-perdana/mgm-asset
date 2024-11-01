a8bb11e2effa9a3e0327ced713afab49655839a1
riyen-perdana <riyenperdana@uin-suska.ac.id>
2024-11-01 10:29:28 +0700

    commit

--- app/Filament/Resources/AssetResource/Widgets/StatsOverview.php
+++ app/Filament/Resources/AssetResource/Widgets/StatsOverview.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\AssetResource\Widgets;
 
 use App\Models\Asset;
 use App\Models\Client;
+use App\Models\Unit;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -13,13 +14,14 @@ class StatsOverview extends BaseWidget
     {
         return [
             Stat::make('Jumlah Asset', Asset::count().' Asset')
-                ->description('Total Asset Keseluruhan')
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->description('Total Asset')
                 ->color('success'),
             Stat::make('Jumlah Relasi', Client::count().' Relasi')
-                ->description('Total Relasi Keseluruhan')
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->color('success')
+                ->description('Total Relasi')
+                ->color('success'),
+            Stat::make('Jumlah Unit', Unit::count().' Unit')
+                ->description('Total Unit')
+                ->color('success'),
         ];
     }
 }
